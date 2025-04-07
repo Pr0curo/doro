cf58bafa3082b5bb7e483c02ad411288ebbd136d
Pr0curo <no@nix.com>
2025-04-07 23:03:38 +0200

    initial commit

---
+++ src/doro/hallo.py
@@ -0,0 +1,7 @@
+"""sagt hallo und gibt drei zurücl"""
+
+def sag_hallo_und_gib_drei_zurueck():
+    """sagt hallo und gibt drei zurück"""
+    print("hallo")
+
+    return 3
---
+++ tests/test_hallo.py
@@ -0,0 +1,10 @@
+"""sach mal so"""
+
+#import pytest
+
+from doro.hallo import sag_hallo_und_gib_drei_zurueck
+
+
+def test_sag_hallo_und_gib_drei_zurueck():
+    """ test mal so"""
+    assert sag_hallo_und_gib_drei_zurueck() == 3
