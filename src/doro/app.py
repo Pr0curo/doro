3493f84be282425ed4087d1f289c94c30482ac71
Pr0curo <no@nix.com>
2025-04-13 00:00:38 +0200

    add \decr path and refactoring code

--- src/doro/app.py
+++ src/doro/app.py
@@ -11,6 +11,12 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     """shows index"""
+
+    return increase_page_views()
+
+@app.get("/incr")
+def increase_page_views():
+    """increases the page views"""
     try:
         page_views = redis().incr("page_views")
     except RedisError:
@@ -18,6 +24,20 @@ def index():
         return "Sorry, something went wrong \N{pensive face}", 500
 
     return f"This page has been seen {page_views} times."
+
+
+
+@app.get("/decr")
+def decrease_page_views():
+    """decreases the page views"""
+    try:
+        page_views = redis().decr("page_views")
+    except RedisError:
+        app.logger.exception("Redis error")
+        return "Sorry, something went wrong \N{pensive face}", 500
+
+    return f"This page has been seen {page_views} times."
+
 
 @cache
 def redis():
